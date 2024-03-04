1997049aee869d2415f2e9e8695545a82a001e9a
Johannes Millan <johannes.millan@gmail.com>
2024-03-04 21:24:10 +0100

    feat: outline dashboard

--- src/content-script/MindedDashboard.tsx
+++ src/content-script/MindedDashboard.tsx
@@ -1,11 +1,59 @@
 import { JSX } from "solid-js";
-import { MindedQuestion } from "@src/content-script/MindedQuestion";
 
 export const MindedDashboard: () => JSX.Element = () => {
   return (
     <>
       <div id="minded-6622-coloured-wrapper">
-        <h1>Minded Dashboard</h1>
+        <div className="box-wrapper">
+          <div className="box">
+            <h2 title="Show all">What is good today</h2>
+            <div className="user-quote">The sun is shining</div>
+          </div>
+          <div className="box">
+            <h2 title="Show all">What you wanted to do</h2>
+            <div className="user-quote">Get my website done</div>
+          </div>
+          <div className="box">
+            <h2 title="Show all">I like about myself</h2>
+            <div className="user-quote">That I am a kind person</div>
+            <div className="user-quote">That I am a good cook</div>
+          </div>
+          <div className="box">
+            <h2 title="Show all">Things I am good at</h2>
+            <div className="user-quote">Motivate others to be goofy.</div>
+          </div>
+          <div className="box">
+            <h2 title="Show all">A strength of mine is</h2>
+            <div className="user-quote">Skiing</div>
+          </div>
+          <div className="box">
+            <h2 title="Show all">Things you like</h2>
+            <div className="user-quote">I like Hannah</div>
+            <div className="user-quote">Sunflowers</div>
+          </div>
+          <div className="box">
+            <h2 title="Show all">I want to be</h2>
+            <div className="user-quote">More relaxed</div>
+            <div className="user-quote">Better focused</div>
+            <div className="user-quote">More sporty</div>
+          </div>
+          <div className="box">
+            <h2 title="Show all">Lorem ipsum</h2>
+            <div className="user-quote">Lorem ipsum dolor sit amet.</div>
+            <div className="user-quote">Lorem ipsum dolor.</div>
+            <div className="user-quote">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolore dolores
+              incidunt inventore sit voluptas
+            </div>
+          </div>
+          <div className="box">
+            <h2 title="Show all">Lorem ipsum</h2>
+            <div className="user-quote">Lorem ipsum dolor sit amet.</div>
+            <div className="user-quote">Lorem ipsum dolor.</div>
+            <div className="user-quote">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolore dolores
+              incidunt inventore sit voluptas
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
