44341b282dd959db3dee6937751e68e16484bad8
Johannes Millan <johannes.millan@gmail.com>
2024-03-05 16:35:35 +0100

    feat: open newtab after extension installation

--- src/pages/background/background.ts
+++ src/pages/background/background.ts
@@ -1,11 +1,13 @@
-// console.log("background loaded");
-// chrome.tabs.create({
-//   url: chrome.runtime.getURL("src/pages/popup/index.html"),
-// });
-//
+import { bro } from "@src/util/browser";
 
-chrome.action.onClicked.addListener(function (tab) {
-  chrome.tabs.create({
-    url: chrome.runtime.getURL("src/pages/newtab/index.html"),
+bro.action.onClicked.addListener(function (tab) {
+  bro.tabs.create({
+    url: bro.runtime.getURL("src/pages/newtab/index.html"),
   });
 });
+bro.runtime.onInstalled.addListener(() => {
+    bro.tabs.create({
+      url: bro.runtime.getURL("src/pages/newtab/index.html"),
+    });
+  }
+);
