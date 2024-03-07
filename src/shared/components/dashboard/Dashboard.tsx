b2644a20748a7593ee093665c0010735d0bca847
Johannes Millan <johannes.millan@gmail.com>
2024-03-07 14:42:47 +0100

    feat: highlight random dashboard entry

--- src/shared/components/dashboard/Dashboard.tsx
+++ src/shared/components/dashboard/Dashboard.tsx
@@ -5,34 +5,40 @@ import { DashboardGroup, DashboardGroupType, } from "@src/shared/components/dash
 import { AnswerList } from "@src/shared/components/dashboard/AnswerList";
 import { RndQuote } from "@src/shared/components/dashboard/RndQuote";
 import { dashboardEntriesFromQuestions } from '@src/shared/components/dashboard/dashboardEntriesFromQuestions';
+import { getRandomInt } from '@src/util/getRndInt';
+
 
 export const Dashboard: () => JSX.Element = () => {
   const [getDashboardGroups, setDashboardGroups] = createSignal<
     DashboardGroup[]
   >([]);
+  let focusElI = 0;
 
   onMount(() => {
     getSyncData().then((syncData) => {
-      if (syncData.answers?.length) {
+      if(syncData.answers?.length) {
         const entries = dashboardEntriesFromQuestions(syncData.answers);
+        focusElI = getRandomInt(0, entries.length - 1);
+        console.log(focusElI);
+
         setDashboardGroups(entries);
       }
     });
   });
 
   return (
-    <div nr-of-items={getDashboardGroups().length} class={styles.Dashboard}>
-      {getDashboardGroups().map((dg) => {
+    <div nr-of-items={getDashboardGroups().length}
+         class={styles.Dashboard}>
+      {getDashboardGroups().map((dg, index) => {
         switch (dg.type) {
           case DashboardGroupType.Quote:
             return (
-              <div className={styles.box}>
-                <RndQuote />
+              <div className={styles.box + ' ' + (index === focusElI ? styles.isFocus : '')}><RndQuote />
               </div>
             );
           default:
             return (
-              <div className={styles.box}>
+              <div className={styles.box + ' ' + (index === focusElI ? styles.isFocus : '')}>
                 <AnswerList dashboardGroup={dg} />
               </div>
             );
