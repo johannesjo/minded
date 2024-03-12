7c4e899245db717545d6b983bb1e784c2683b93e
Johannes Millan <johannes.millan@gmail.com>
2024-03-12 15:08:47 +0100

    feat: improve select question algorithm

--- src/util/getQuestionSmart.ts
+++ src/util/getQuestionSmart.ts
@@ -1,23 +1,41 @@
-import { Answer } from '@src/shared/data/sync-data';
-import { QuestionForPrompt, QUESTIONS } from '@src/shared/data/questions';
-import { getRndEntry } from '@src/util/getRndEntry';
+import { Answer } from "@src/shared/data/sync-data";
+import {
+  QUESTION_CATEGORIES,
+  QuestionForPrompt,
+  QUESTIONS,
+} from "@src/shared/data/questions";
+import { getRndEntry } from "@src/util/getRndEntry";
+import { isThisWeek, isToday } from "@src/util/isToday";
 
 export const getQuestionSmart = (answers: Answer[]): QuestionForPrompt => {
-  if(!answers.length) {
+  const now = new Date();
+  if (!answers.length) {
     return getRndEntry(QUESTIONS);
   }
 
-  const map = {};
-  answers.forEach(answer => {
+  const map: { [key: string]: number } = {};
+  answers.forEach((answer) => {
+    const categoryForAnswer = QUESTION_CATEGORIES[answer.questionCategoryId];
+    if (categoryForAnswer.isTodayOnlyCategory && !isToday(answer.ts)) {
+      return;
+    }
+    if (categoryForAnswer.isThisWeekOnlyCategory && !isThisWeek(answer.ts)) {
+      return;
+    }
+
     map[answer.questionCategoryId] = map[answer.questionCategoryId]
       ? map[answer.questionCategoryId] + 1
       : 1;
   });
   const sortedEntries = Object.entries(map).sort((a, b) => a[1] - b[1]);
   const nrOfEntriesForLeastUsed = sortedEntries[0][1];
-  const categoriesLeastUsed = sortedEntries.filter(se => se[1] === nrOfEntriesForLeastUsed);
+  const categoriesLeastUsed = sortedEntries.filter(
+    (se) => se[1] === nrOfEntriesForLeastUsed,
+  );
   const categoryToUse = getRndEntry(categoriesLeastUsed)[0];
-  const questionsForCategory = QUESTIONS.filter(q => q.categoryId === categoryToUse);
+  const questionsForCategory = QUESTIONS.filter(
+    (q) => q.categoryId === categoryToUse,
+  );
   // console.log({sortedEntries, nrOfEntriesForLeastUsed, categoriesLeastUsed, categoryToUse, questionsForCategory});
   return getRndEntry(questionsForCategory);
 };
