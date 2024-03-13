f2974718d0d3180857c09ff7fe832c9503db885f
Johannes Millan <johannes.millan@gmail.com>
2024-03-13 12:40:23 +0100

    refactor: split dashboard groups into their own component

--- src/shared/components/dashboard/Dashboard.tsx
+++ src/shared/components/dashboard/Dashboard.tsx
@@ -1,64 +1,6 @@
-import { createSignal, JSX, onMount } from "solid-js";
-import styles from "./Dashboard.module.scss";
-import { getSyncData } from "@src/shared/data/dataInterface";
-import {
-  DashboardGroup,
-  DashboardGroupType,
-} from "@src/shared/components/dashboard/dashboard.model";
-import { AnswerList } from "@src/shared/components/dashboard/AnswerList";
-import { RndQuote } from "@src/shared/components/dashboard/RndQuote";
-import { dashboardEntriesFromQuestions } from "@src/shared/components/dashboard/dashboardEntriesFromQuestions";
-import { QuestionCategoryId } from "@src/shared/data/questions";
-import Rating from "@src/shared/components/ui/Rating";
+import { JSX } from "solid-js";
+import { DashboardGroups } from "@src/shared/components/dashboard/DashboardGroups";
 
 export const Dashboard: () => JSX.Element = () => {
-  const [getDashboardGroups, setDashboardGroups] = createSignal<
-    DashboardGroup[]
-  >([]);
-
-  onMount(() => {
-    getSyncData().then((syncData) => {
-      if (syncData.answers?.length) {
-        const entries = dashboardEntriesFromQuestions(syncData.answers);
-        setDashboardGroups(entries);
-      }
-    });
-  });
-
-  return (
-    <div nr-of-items={getDashboardGroups().length} class={styles.Dashboard}>
-      {getDashboardGroups().map((dg, index) => {
-        switch (dg.type) {
-          case DashboardGroupType.Quote:
-            return (
-              <div class={styles.box}>
-                <RndQuote />
-              </div>
-            );
-          default:
-            switch (dg.id) {
-              case QuestionCategoryId.XEnergyLevelToday:
-                return (
-                  <div class={styles.box}>
-                    <div class={styles.standardHeading}>
-                      Your Energy Level Today
-                    </div>
-                    <Rating
-                      isShowOnly={true}
-                      value={(dg.answers[0] && dg.answers[0].val) as number}
-                    />
-                  </div>
-                );
-
-              default:
-                return (
-                  <div class={styles.box}>
-                    <AnswerList dashboardGroup={dg} />
-                  </div>
-                );
-            }
-        }
-      })}
-    </div>
-  );
+  return <DashboardGroups />;
 };
---
+++ src/shared/components/dashboard/DashboardGroups.tsx
@@ -0,0 +1,67 @@
+import { createSignal, JSX, onMount } from "solid-js";
+import {
+  DashboardGroup,
+  DashboardGroupType,
+} from "@src/shared/components/dashboard/dashboard.model";
+import { getSyncData } from "@src/shared/data/dataInterface";
+import { dashboardEntriesFromQuestions } from "@src/shared/components/dashboard/dashboardEntriesFromQuestions";
+import styles from "@src/shared/components/dashboard/DashboardGroups.module.scss";
+import { RndQuote } from "@src/shared/components/dashboard/RndQuote";
+import { QuestionCategoryId } from "@src/shared/data/questions";
+import Rating from "@src/shared/components/ui/Rating";
+import { AnswerList } from "@src/shared/components/dashboard/AnswerList";
+
+export const DashboardGroups: () => JSX.Element = () => {
+  const [getDashboardGroups, setDashboardGroups] = createSignal<
+    DashboardGroup[]
+  >([]);
+
+  onMount(() => {
+    getSyncData().then((syncData) => {
+      if (syncData.answers?.length) {
+        const entries = dashboardEntriesFromQuestions(syncData.answers);
+        setDashboardGroups(entries);
+      }
+    });
+  });
+
+  return (
+    <div
+      nr-of-items={getDashboardGroups().length}
+      class={styles.DashboardGroups}
+    >
+      {getDashboardGroups().map((dg, index) => {
+        switch (dg.type) {
+          case DashboardGroupType.Quote:
+            return (
+              <div class={styles.box}>
+                <RndQuote />
+              </div>
+            );
+          default:
+            switch (dg.id) {
+              case QuestionCategoryId.XEnergyLevelToday:
+                return (
+                  <div class={styles.box}>
+                    <div class={styles.standardHeading}>
+                      Your Energy Level Today
+                    </div>
+                    <Rating
+                      isShowOnly={true}
+                      value={(dg.answers[0] && dg.answers[0].val) as number}
+                    />
+                  </div>
+                );
+
+              default:
+                return (
+                  <div class={styles.box}>
+                    <AnswerList dashboardGroup={dg} />
+                  </div>
+                );
+            }
+        }
+      })}
+    </div>
+  );
+};
