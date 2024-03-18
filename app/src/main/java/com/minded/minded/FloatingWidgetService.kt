f30c11d8908d9cf4fc5b9f2c1991e28d52de7b79
Johannes Millan <johannes.millan@gmail.com>
2024-03-18 16:59:57 +0100

    feat: make overlay work with compose

---
+++ app/src/main/java/com/minded/minded/FloatingWidgetService.kt
@@ -0,0 +1,167 @@
+package com.minded.minded
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.PixelFormat
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.view.WindowManager
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.setViewTreeLifecycleOwner
+import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryController
+import androidx.savedstate.SavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import com.minded.minded.ui.theme.MindedTheme
+
+class FloatingWidgetService : Service(),
+    LifecycleOwner,
+    SavedStateRegistryOwner {
+
+    lateinit var windowManager: WindowManager
+    private val _lifecycleRegistry = LifecycleRegistry(this)
+    private val _savedStateRegistryController: SavedStateRegistryController =
+        SavedStateRegistryController.create(this)
+    override val savedStateRegistry: SavedStateRegistry =
+        _savedStateRegistryController.savedStateRegistry
+    override val lifecycle: Lifecycle = _lifecycleRegistry
+    private var overlayView: View? = null
+
+
+    override fun onCreate() {
+        super.onCreate()
+        windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        _savedStateRegistryController.performAttach()
+        _savedStateRegistryController.performRestore(null)
+        _lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+
+        showOverlay()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        throw RuntimeException("bound mode not supported")
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        if (intent.hasExtra(INTENT_EXTRA_COMMAND_SHOW_OVERLAY)) {
+            showOverlay()
+        }
+        if (intent.hasExtra(INTENT_EXTRA_COMMAND_HIDE_OVERLAY)) {
+            hideOverlay()
+        }
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        hideOverlay()
+        _lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    }
+
+    private fun showOverlay() {
+        Log.v("SVC", "showOverlay()")
+        if (overlayView != null) {
+            Log.v("SVC", "overlay already shown - aborting")
+            return
+        }
+        _lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
+        _lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        overlayView = ComposeView(this).apply {
+            setViewTreeLifecycleOwner(this@FloatingWidgetService)
+            setViewTreeSavedStateRegistryOwner(this@FloatingWidgetService)
+            setContent {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Column {
+                        Text("Hello, Compose!")
+                        Button(onClick = { /*TODO*/ }) {
+                            Text("Click me")
+                        }
+                    }
+                }
+            }
+        }
+        windowManager.addView(overlayView, getLayoutParams())
+    }
+
+    private fun hideOverlay() {
+        Log.v(
+            "SVC", "hideOverlay()"
+        )
+        if (overlayView == null) {
+            Log.v(
+                "SVC", "overlay not shown - aborting"
+            )
+            return
+        }
+        windowManager.removeView(overlayView)
+        overlayView = null
+        _lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        _lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
+    }
+
+    private fun getLayoutParams(): WindowManager.LayoutParams {
+        return WindowManager.LayoutParams(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+            PixelFormat.TRANSLUCENT
+        )
+    }
+
+    companion object {
+        private const val INTENT_EXTRA_COMMAND_SHOW_OVERLAY = "INTENT_EXTRA_COMMAND_SHOW_OVERLAY"
+        private const val INTENT_EXTRA_COMMAND_HIDE_OVERLAY = "INTENT_EXTRA_COMMAND_HIDE_OVERLAY"
+        internal fun showOverlay(context: Context) {
+            val intent = Intent(context, FloatingWidgetService::class.java)
+            intent.putExtra(INTENT_EXTRA_COMMAND_SHOW_OVERLAY, true)
+            context.startService(intent)
+        }
+
+        internal fun hideOverlay(context: Context) {
+            val intent = Intent(context, FloatingWidgetService::class.java)
+            intent.putExtra(INTENT_EXTRA_COMMAND_HIDE_OVERLAY, true)
+            context.startService(intent)
+        }
+    }
+}
+
+
+@Composable
+fun Widget() {
+    Surface(color = Color.Magenta) {
+        Column {
+            // Replace this with your own Compose UI
+            Text("Hello, Compose!")
+            Button(onClick = { /*TODO*/ }) {
+
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun WidgetPreview() {
+    Widget()
+}
