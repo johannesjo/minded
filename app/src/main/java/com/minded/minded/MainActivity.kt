7cc15e14259affa48263146e703f0671a68ea8ed
Johannes Millan <johannes.millan@gmail.com>
2024-03-18 16:59:57 +0100

    feat: make overlay work with compose

--- app/src/main/java/com/minded/minded/MainActivity.kt
+++ app/src/main/java/com/minded/minded/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.minded.minded
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,9 +19,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.minded.minded.ui.theme.MindedTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private val SYSTEM_ALERT_WINDOW_PERMISSION = 2084
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.v("MAIN", "ON_CREATE MAIN ACTIVITY")
+
+        startService(Intent(this, FloatingWidgetService::class.java))
+//        startService(Intent(this, com.minded.minded.FloatingWidgetService::class.java))
+
         setContent {
             MindedTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,10 +35,47 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("AndrAAaaaaAoid")
                 }
             }
         }
+
+        if (Settings.canDrawOverlays(this)) {
+            Toast.makeText(
+                this,
+                "START SERVICE",
+                Toast.LENGTH_SHORT
+            ).show()
+//            startService(Intent(this, FloatingWidgetService::class.java))
+//            finish()
+        } else {
+            Toast.makeText(
+                this,
+                "You need System Alert Window Permission to do this",
+                Toast.LENGTH_SHORT
+            ).show()
+            askPermission()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == SYSTEM_ALERT_WINDOW_PERMISSION) {
+            if (Settings.canDrawOverlays(this)) {
+                startService(Intent(this, FloatingWidgetService::class.java))
+            } else {
+                Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun askPermission() {
+        val intent = Intent(
+            Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+            Uri.parse("package:$packageName")
+        )
+        startActivityForResult(intent, SYSTEM_ALERT_WINDOW_PERMISSION)
     }
 }
 
