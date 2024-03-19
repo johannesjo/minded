7c42e84617c2af8a2b4915a5c3f9da267197f9da
Johannes Millan <johannes.millan@gmail.com>
2024-03-19 10:43:54 +0100

    feat: make permission stuff work

--- app/src/main/java/com/minded/minded/MyUtil.kt
+++ app/src/main/java/com/minded/minded/MyUtil.kt
@@ -1,8 +1,11 @@
 package com.minded.minded
 
+import android.app.AppOpsManager
 import android.app.usage.UsageStatsManager
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
+import android.provider.Settings
 
 object MyUtil {
     fun checkPermission(context: Context, permission: String): Boolean {
@@ -10,11 +13,41 @@ object MyUtil {
         return res == PackageManager.PERMISSION_GRANTED
     }
 
+    fun checkUsageStatsPermission(context: Context): Boolean {
+        val granted: Boolean
+        val appOps = context.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOps.checkOpNoThrow(
+            AppOpsManager.OPSTR_GET_USAGE_STATS,
+            android.os.Process.myUid(),
+            context.packageName
+        )
+        granted = if (mode == AppOpsManager.MODE_DEFAULT) {
+            context.checkCallingOrSelfPermission(android.Manifest.permission.PACKAGE_USAGE_STATS) == PackageManager.PERMISSION_GRANTED
+        } else {
+            mode == AppOpsManager.MODE_ALLOWED
+        }
+
+        return granted
+    }
+
+    fun checkDrawOverlayPermission(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Settings.canDrawOverlays(context)
+        } else {
+            true
+        }
+    }
+
     fun getForegroundApp(context: Context): String {
-        val usageStatsManager = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val usageStatsManager =
+            context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
 
         val time = System.currentTimeMillis()
-        val usageStatsList = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 3600, time)
+        val usageStatsList = usageStatsManager.queryUsageStats(
+            UsageStatsManager.INTERVAL_DAILY,
+            time - 1000 * 3600,
+            time
+        )
 
         var foregroundApp = ""
         var lastUsedAppTime = 0L
