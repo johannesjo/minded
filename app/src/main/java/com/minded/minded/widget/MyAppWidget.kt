aa108a76d9798c691a386f817a832017b11b017c
Johannes Millan <johannes.millan@gmail.com>
2024-03-20 21:03:30 +0100

    feat: add widget boilerplate

---
+++ app/src/main/java/com/minded/minded/widget/MyAppWidget.kt
@@ -0,0 +1,53 @@
+package com.minded.minded.widget
+
+import android.content.Context
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.glance.Button
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.action.actionStartActivity
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import com.minded.minded.MainActivity
+
+
+class MyAppWidget : GlanceAppWidget() {
+
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        // Load data needed to render the AppWidget.
+        // Use `withContext` to switch to another thread for long running
+        // operations.
+
+        provideContent {
+            // create your AppWidget here
+            MyContent()
+        }
+    }
+
+    @Composable
+    private fun MyContent() {
+        Column(
+//            modifier = GlanceModifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+//            Text(text = "Where to?", modifier = GlanceModifier.padding(12.dp))
+            Row() {
+                Button(
+                    text = "Home",
+                    onClick = actionStartActivity<MainActivity>()
+                )
+                Button(
+                    text = "Work",
+                    onClick = actionStartActivity<MainActivity>()
+                )
+            }
+        }
+    }
+}
