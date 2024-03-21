f3d313fefbaf40495cec28c2e0fc07cc6e12adf0
Johannes Millan <johannes.millan@gmail.com>
2024-03-21 11:05:22 +0100

    feat: polish widget a little

--- app/src/main/java/com/minded/minded/widget/MyAppWidget.kt
+++ app/src/main/java/com/minded/minded/widget/MyAppWidget.kt
@@ -2,23 +2,47 @@ package com.minded.minded.widget
 
 import android.content.Context
 import android.util.Log
+import androidx.compose.foundation.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.appWidgetBackground
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
 import androidx.glance.layout.Box
 import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxHeight
+import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.padding
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
 import com.minded.minded.MyUtil
 import com.minded.minded.data.QuestionCategoryForDashboard
 import com.minded.minded.data.answers.AnswerRepository
+import com.minded.minded.ui.theme.PastelYellow
+import com.minded.minded.ui.theme.StandardGradient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.content.Intent
+import android.os.SystemClock
 
 
 class MyAppWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
+        Log.v("MyAppWidget", "provideGlance")
+
         val answerRepository = AnswerRepository(context)
 
         // Use the repository to fetch data
@@ -39,21 +63,30 @@ class MyAppWidget : GlanceAppWidget() {
             QuestionCategoryCmp2(questionDataForDashboard.random())
         }
     }
+
 }
 
 
 @Composable
 fun QuestionCategoryCmp2(question: QuestionCategoryForDashboard) {
     Box(
+        modifier = GlanceModifier
+            .background(
+                color = PastelYellow
+            ).padding(16.dp)
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .appWidgetBackground()
     ) {
         Column(
         ) {
 
             Text(
                 text = question.dashboardTxt ?: "No text",
+                style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold)
             )
             question.answers.forEach {
-                Text(it.txt)
+                Text(it.txt, modifier = GlanceModifier.padding(top= 8.dp))
             }
         }
     }
