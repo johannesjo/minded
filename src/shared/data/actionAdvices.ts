3be13cb23ade372023aeac2c0437a2a8b88badf3
Johannes Millan <johannes.millan@gmail.com>
2024-03-25 14:49:05 +0100

    feat: add new action advice

--- src/shared/data/actionAdvices.ts
+++ src/shared/data/actionAdvices.ts
@@ -1,10 +1,12 @@
 export const ACTION_ADVICES: { ico?: string; txt: string }[] = [
-  {ico: '✿', txt: "How about looking out the window for a minute?"},
-  {ico: '☕', txt: "How about a short break?"},
-  {ico: '🏅', txt: "How about a little stretch?"},
-  // {ico: '☀', txt: "How about some fresh air?"},
-  {ico: '🌞', txt: "How about some fresh air?"},
-  {ico: '࿋', txt: "How about a deep breath?"},
-  {ico: '🌊', txt: "How about some water?"},
+  { ico: "✿", txt: "How about looking out the window for a minute?" },
+  { ico: "☕", txt: "How about a short break?" },
+  { ico: "🏅", txt: "How about a little stretch?" },
+  { ico: "🌞", txt: "How about some fresh air?" },
+  { ico: "࿋", txt: "How about a deep breath?" },
+  { ico: "🌊", txt: "How about some water?" },
+  {
+    ico: "🤔",
+    txt: "Sometimes procrastination is an indicator that you actually should be doing something else than the task you are avoiding...",
+  },
 ] as const;
-
