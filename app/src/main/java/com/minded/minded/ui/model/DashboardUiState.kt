47c60e121d7762c560346d53c8840ad21529ab76
Johannes Millan <johannes.millan@gmail.com>
2024-03-27 14:55:23 +0100

    feat: add add button to dashboard and improve capability handling

--- app/src/main/java/com/minded/minded/ui/model/DashboardUiState.kt
+++ app/src/main/java/com/minded/minded/ui/model/DashboardUiState.kt
@@ -1,9 +1,9 @@
 package com.minded.minded.ui.model
 
-import com.minded.minded.data.QuestionCategory
+import com.minded.minded.MissingCapability
 import com.minded.minded.data.QuestionCategoryForDashboard
 
 data class DashboardUiState(
     val questionCategories: List<QuestionCategoryForDashboard> = emptyList(),
-    val missingCapability: String = ""
+    val missingCapability: MissingCapability? = null
 ) {}
