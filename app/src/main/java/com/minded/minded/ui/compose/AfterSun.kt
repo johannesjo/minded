1eec5d2f815f7cf2aede4123cb58b4f35d8e29ee
Johannes Millan <johannes.millan@gmail.com>
2024-04-08 16:53:24 +0200

    feat: add attention seeking animation for little sun

--- app/src/main/java/com/minded/minded/ui/compose/AfterSun.kt
+++ app/src/main/java/com/minded/minded/ui/compose/AfterSun.kt
@@ -1,5 +1,12 @@
 package com.minded.minded.ui.compose
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.StartOffset
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -8,8 +15,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -24,43 +33,83 @@ fun AfterSun(elapsedSeconds: Int = 0, onSunTap: () -> Unit = {}) {
     val minutes = elapsedSeconds / 60
     val remainingSeconds = elapsedSeconds % 60
     val clockString = String.format("%2d:%02d", minutes, remainingSeconds)
+    val pulseAniDuration = 1000
+    val pulseColorChangeAniDuration = 2000
+    val cycleDuration = 4000
+
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 1.3f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(pulseAniDuration, cycleDuration - pulseAniDuration),
+            repeatMode = RepeatMode.Reverse,
+            StartOffset(pulseColorChangeAniDuration / 2)
+        )
+    )
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.White,
+        targetValue = Color(0xFFFFEDCA),
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                pulseColorChangeAniDuration,
+                cycleDuration - pulseColorChangeAniDuration
+            ),
+            repeatMode = RepeatMode.Reverse,
+        )
+    )
+
+
+
+
 
     Box(
         modifier = Modifier
-            .size(44.dp),
+//            .border(1.dp, Color.Black, CircleShape  )
+            .size(52.dp),
         contentAlignment = Alignment.Center // This will center the inner Box
 
     ) {
-        val brush = Brush.radialGradient(listOf(Color.Red, Color.Transparent))
-        Canvas(
-            modifier = Modifier.size(100.dp),
-            onDraw = {
-                drawCircle(brush)
-            }
-        )
-
         Box(
             modifier = Modifier
-                .size(36.dp)
-                .clickable(onClick = onSunTap)
+//                .border(1.dp, Color.Red, CircleShape  )
+                .size(44.dp),
+            contentAlignment = Alignment.Center // This will center the inner Box
+
         ) {
-            Surface(
-                shape = CircleShape,
-                color = Color.White,
+            val brush = Brush.radialGradient(listOf(Color.Red, Color.Transparent))
+            Canvas(
+                modifier = Modifier.size(100.dp),
+                onDraw = {
+                    drawCircle(brush)
+                }
+            )
+
+            Box(
                 modifier = Modifier
-                    .matchParentSize()
+                    .scale(scale)
+                    .size(36.dp)
+                    .clickable(onClick = onSunTap)
+            ) {
+                Surface(
+                    shape = CircleShape,
+                    color = color,
+                    modifier = Modifier
+                        .matchParentSize()
 //                    .shadow(elevation = 4.dp, shape = CircleShape) // Add shadow here
 
-            ) {
+                ) {
 
-                Box(contentAlignment = androidx.compose.ui.Alignment.Center) {
-                    Text(
-                        text = clockString,
-                        fontSize = 12.sp,
-                        textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.Normal,
-                        maxLines = 1
-                    )
+                    Box(contentAlignment = androidx.compose.ui.Alignment.Center) {
+                        Text(
+                            text = clockString,
+                            fontSize = 12.sp,
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.Normal,
+                            maxLines = 1
+                        )
+                    }
                 }
             }
         }
