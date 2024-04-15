06928b81a5d6d458d8afe77f43cc58388e77dc0c
Johannes Millan <johannes.millan@gmail.com>
2024-04-15 16:53:53 +0200

    feat: add insomnia questions

--- app/src/main/java/com/minded/minded/util/getQuestionSmart.kt
+++ app/src/main/java/com/minded/minded/util/getQuestionSmart.kt
@@ -1,20 +1,19 @@
 package com.minded.minded.util
 
-import android.text.format.DateUtils
 import android.text.format.DateUtils.isToday
 import com.minded.minded.data.QUESTIONS
 import com.minded.minded.data.QUESTION_CATEGORIES
 import com.minded.minded.data.QuestionCategoryId
 import com.minded.minded.data.QuestionForPrompt
 import com.minded.minded.data.answers.Answer
-
-import java.util.*
-import kotlin.collections.HashMap
+import java.util.Calendar
 
 val THRESHOLD_MORNING_START = 4
 val THRESHOLD_MORNING_END = 14
 val THRESHOLD_EVENING_START = 15
 val FAKE_RULE_OUT_NR = 9999
+val THRESHOLD_LATE_NIGHT_START = 0
+val THRESHOLD_LATE_NIGHT_END = 4
 
 fun getQuestionSmart(answers: List<Answer>): QuestionForPrompt {
     val now = Calendar.getInstance()
@@ -41,6 +40,11 @@ fun getQuestionSmart(answers: List<Answer>): QuestionForPrompt {
         }
         if (categoryForAnswer.isEveningCategory) {
             if (nowHours < THRESHOLD_EVENING_START) {
+                map[categoryId] = FAKE_RULE_OUT_NR
+            }
+        }
+        if (categoryForAnswer.isLateNightCategory) {
+            if (nowHours < THRESHOLD_LATE_NIGHT_START || nowHours > THRESHOLD_LATE_NIGHT_END) {
                 map[categoryId] = FAKE_RULE_OUT_NR
             }
         }
