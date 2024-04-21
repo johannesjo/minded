5b13dce7a082e1210a7a993ac9c61dd34ebb69a8
Johannes Millan <johannes.millan@gmail.com>
2024-04-21 17:53:53 +0200

    feat: show questionTxt for each answer on hover

--- src/shared/components/dashboard/AnswerList.tsx
+++ src/shared/components/dashboard/AnswerList.tsx
@@ -3,6 +3,7 @@ import { JSX } from "solid-js";
 import styles from "./AnswerList.module.scss";
 import { DashboardGroupStandard } from "@src/shared/components/dashboard/dashboard.model";
 import { truncate } from "@src/util/truncate";
+import { QUESTIONS } from "@src/shared/data/questions";
 
 const MAX_ANSWER_LENGTH = 200;
 
@@ -29,18 +30,23 @@ export const AnswerList: (props: {
       {/*  onAdd={() => undefined}*/}
       {/*/>*/}
 
-      {props.dashboardGroup.answers.map((answer) => (
-        <div
-          class={styles.userQuote}
-          title={
-            answer.val.toString().length > MAX_ANSWER_LENGTH
-              ? answer.val.toString()
-              : ""
-          }
-        >
-          {truncate(answer.val.toString(), MAX_ANSWER_LENGTH)}
-        </div>
-      ))}
+      {props.dashboardGroup.answers.map((answer) => {
+        const questionTxt = answer.qid
+          ? `Question: ${QUESTIONS.find((q) => q.id === answer.qid).t}?`
+          : "";
+        const titleTxt =
+          answer.val.toString().length > MAX_ANSWER_LENGTH
+            ? answer.qid
+              ? `${answer.val.toString()} – ${questionTxt}`
+              : answer.val.toString()
+            : questionTxt;
+
+        return (
+          <div class={styles.userQuote} title={titleTxt}>
+            {truncate(answer.val.toString(), MAX_ANSWER_LENGTH)}
+          </div>
+        );
+      })}
     </div>
   );
 };
