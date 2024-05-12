7c358f42c784b0054fecb0a9b744aa579a83b343
Johannes Millan <johannes.millan@gmail.com>
2024-05-12 12:55:04 +0200

    feat: web view half way working

--- app/src/main/java/com/minded/minded/MainActivity.kt
+++ app/src/main/java/com/minded/minded/MainActivity.kt
@@ -7,19 +7,16 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.minded.minded.data.answers.AnswerRepository
-import com.minded.minded.ui.compose.Dashboard
 import com.minded.minded.ui.model.DashboardViewModel
 import com.minded.minded.ui.model.DashboardViewModelFactory
-import com.minded.minded.ui.theme.MindedTheme
 import com.minded.minded.util.checkDrawOverlayPermission
 import com.minded.minded.util.isAccessibilityServiceEnabled
 import kotlinx.coroutines.launch
@@ -45,23 +42,39 @@ class MainActivity : AppCompatActivity() {
 
         lifecycleScope.launch {
             setContent {
-                MindedTheme {
-                    Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
-                    ) {
-                        Dashboard(
-                            dashboardViewModel,
-                            onMissingCapabilityClick = {
-                                Log.v("MAIN", "onMissingCapabilityClick() $it")
-                                when (it) {
-                                    MissingCapability.Accessibility -> askPermissionForAccessibility()
-                                    MissingCapability.SystemAlertWindow -> askPermissionForOverlay()
-                                }
-                            },
+                AndroidView(factory = { context ->
+                    WebView(context).apply {
+
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
                         )
+                        settings.javaScriptEnabled = true
+                        settings.allowFileAccess = true
+                        settings.allowFileAccessFromFileURLs = true
+                        settings.allowUniversalAccessFromFileURLs = true
+                        settings.allowContentAccess = true
+                        loadUrl("file:///android_asset/web/src/android/main/index.html")
                     }
-                }
+                })
+
+//                MindedTheme {
+//                    Surface(
+//                        modifier = Modifier.fillMaxSize(),
+//                        color = MaterialTheme.colorScheme.background
+//                    ) {
+//                        Dashboard(
+//                            dashboardViewModel,
+//                            onMissingCapabilityClick = {
+//                                Log.v("MAIN", "onMissingCapabilityClick() $it")
+//                                when (it) {
+//                                    MissingCapability.Accessibility -> askPermissionForAccessibility()
+//                                    MissingCapability.SystemAlertWindow -> askPermissionForOverlay()
+//                                }
+//                            },
+//                        )
+//                    }
+//                }
             }
         }
     }
