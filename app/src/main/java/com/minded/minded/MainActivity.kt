b40ac475b5258598bcb61383b57ea08f4334c228
Johannes Millan <johannes.millan@gmail.com>
2024-05-14 13:13:32 +0200

    feat: make status bar color black

--- app/src/main/java/com/minded/minded/MainActivity.kt
+++ app/src/main/java/com/minded/minded/MainActivity.kt
@@ -9,11 +9,16 @@ import android.provider.Settings
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.minded.minded.ui.compose.Dashboard
 import com.minded.minded.ui.model.DashboardViewModel
 import com.minded.minded.ui.model.DashboardViewModelFactory
+import com.minded.minded.ui.theme.MindedTheme
 import com.minded.minded.util.checkDrawOverlayPermission
 import com.minded.minded.util.isAccessibilityServiceEnabled
 import kotlinx.coroutines.launch
@@ -37,16 +42,23 @@ class MainActivity : AppCompatActivity() {
 
         lifecycleScope.launch {
             setContent {
-                Dashboard(
-                    dashboardViewModel,
-                    onMissingCapabilityClick = {
-                        Log.v("MAIN", "onMissingCapabilityClick() $it")
-                        when (it) {
-                            MissingCapability.Accessibility -> askPermissionForAccessibility()
-                            MissingCapability.SystemAlertWindow -> askPermissionForOverlay()
-                        }
-                    },
-                )
+                MindedTheme {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        Dashboard(
+                            dashboardViewModel,
+                            onMissingCapabilityClick = {
+                                Log.v("MAIN", "onMissingCapabilityClick() $it")
+                                when (it) {
+                                    MissingCapability.Accessibility -> askPermissionForAccessibility()
+                                    MissingCapability.SystemAlertWindow -> askPermissionForOverlay()
+                                }
+                            },
+                        )
+                    }
+                }
             }
         }
     }
