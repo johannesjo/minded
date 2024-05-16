c95a795f24c55e0a66c704edb1ca0bc7eb0a9191
Johannes Millan <johannes.millan@gmail.com>
2024-05-16 15:10:10 +0200

    feat: style and improve graph

--- src/shared/addDayTimeDependentClass.ts
+++ src/shared/addDayTimeDependentClass.ts
@@ -2,10 +2,15 @@ const DARK_MODE_START_HOUR = 19;
 const DARK_MODE_END_HOUR = 6;
 
 export const addDayTimeDependentClass = () => {
+  if (isDarkModeNow()) {
+    document.getElementById("minded-6622").classList.add("minded-6622-dark");
+  }
+};
+
+export const isDarkModeNow = (): boolean => {
+  // return true;
   const now = new Date();
   const nowHours = now.getHours();
 
-  if (nowHours >= DARK_MODE_START_HOUR || nowHours < DARK_MODE_END_HOUR) {
-    document.getElementById("minded-6622").classList.add("minded-6622-dark");
-  }
+  return nowHours >= DARK_MODE_START_HOUR || nowHours < DARK_MODE_END_HOUR;
 };
