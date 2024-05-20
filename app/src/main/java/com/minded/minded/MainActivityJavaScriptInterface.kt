e2a09fb24b44bf1c741c896e2dd3b5bf957fbdf0
Johannes Millan <johannes.millan@gmail.com>
2024-05-20 11:40:14 +0200

    feat: get list of installed apps

--- app/src/main/java/com/minded/minded/MainActivityJavaScriptInterface.kt
+++ app/src/main/java/com/minded/minded/MainActivityJavaScriptInterface.kt
@@ -1,14 +1,20 @@
 import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.util.Log
 import android.webkit.JavascriptInterface
+import org.json.JSONArray
+import org.json.JSONObject
 
-class MainActivityJavaScriptInterface(
-    private val context: Context,
+open class MainActivityJavaScriptInterface(
+    protected val context: Context,
 ) {
     private val sharedPreferences = context.getSharedPreferences("mindedData", Context.MODE_PRIVATE)
-    val logTag = "MainActivityJavaScriptInterface"
+    var logTag = "MainActivityJavaScriptInterface"
 
     @JavascriptInterface
     fun saveString(key: String, value: String) {
+        Log.v(logTag, "saveString() $value")
         with(sharedPreferences.edit()) {
             putString(key, value)
             apply()
@@ -17,6 +23,32 @@ class MainActivityJavaScriptInterface(
 
     @JavascriptInterface
     fun retrieveString(key: String): String? {
+        Log.v(logTag, "retrieveString() $key")
         return sharedPreferences.getString(key, null)
+    }
+
+    @JavascriptInterface
+    fun getAllApps(): String {
+        val packageManager = context.packageManager
+        val allApps = getInstalledApps()
+        val jsonArray = JSONArray()
+        for (app in allApps) {
+            val jsonObject = JSONObject()
+            jsonObject.put("packageName", app.packageName)
+            jsonObject.put("name", app.loadLabel(context.packageManager).toString())
+            jsonArray.put(jsonObject)
+        }
+        return jsonArray.toString()
+    }
+
+    private fun getInstalledApps(): List<ApplicationInfo> {
+        val packageManager = context.packageManager
+        val allApps = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+        return allApps.filter { app ->
+            (app.flags and ApplicationInfo.FLAG_SYSTEM == 0) || app.packageName in listOf(
+                "com.android.chrome",
+                "com.google.android.youtube"
+            )
+        }
     }
 }
