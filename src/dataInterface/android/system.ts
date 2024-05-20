135bb3d59740b09d2e54709337eaddb0c5a5569a
Johannes Millan <johannes.millan@gmail.com>
2024-05-20 18:29:36 +0200

    refactor: cleanup

--- src/dataInterface/android/system.ts
+++ src/dataInterface/android/system.ts
@@ -3,18 +3,3 @@ import { androidInterface } from "@src/dataInterface/android/androidInterface";
 export const requestFocusAndShowKeyboard = () => {
   androidInterface.requestFocusAndShowKeyboard();
 };
-
-// TODO make more robust
-export const goSettings = (): void => {
-  window.location.href = window.location.href.replace(
-    "main/index.html",
-    "settings/index.html",
-  );
-};
-
-export const goMain = (): void => {
-  window.location.href = window.location.href.replace(
-    "settings/index.html",
-    "main/index.html",
-  );
-};
