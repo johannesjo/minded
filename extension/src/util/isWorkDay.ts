a909822a9773b03ed5f4f2ef425589e12ba57197
Johannes Millan <johannes.millan@gmail.com>
2024-05-26 12:06:20 +0200

    fix: type

--- extension/src/util/isWorkDay.ts
+++ extension/src/util/isWorkDay.ts
@@ -1,4 +1,4 @@
-export const isWorkDay = (date: Date = new Date()): Boolean => {
+export const isWorkDay = (date: Date = new Date()): boolean => {
   const dayOfWeek = date.getDay();
   return dayOfWeek >= 1 && dayOfWeek <= 5;
 };
