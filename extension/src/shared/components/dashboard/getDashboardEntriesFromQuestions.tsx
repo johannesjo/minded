48909d550b251c1addcaf7fac6881debde7fde0a
Johannes Millan <johannes.millan@gmail.com>
2024-05-27 14:56:59 +0200

    feat: don't highlight random entry on android

--- extension/src/shared/components/dashboard/getDashboardEntriesFromQuestions.tsx
+++ extension/src/shared/components/dashboard/getDashboardEntriesFromQuestions.tsx
@@ -5,25 +5,27 @@ import {
   DashboardGroupEnergyLvl,
   DashboardGroupMood,
   DashboardGroupStats,
-  DashboardGroupType,
+  DashboardGroupType
 } from "@src/shared/components/dashboard/dashboard.model";
 import {
   FIXED_QUESTION_CATEGORIES_ON_DASHBOARD,
   QUESTION_CATEGORIES,
   QuestionCategoryId,
-  RANDOM_QUESTION_CATEGORIES_ON_DASHBOARD,
+  RANDOM_QUESTION_CATEGORIES_ON_DASHBOARD
 } from "@src/shared/data/questions";
 import { getRndEntries } from "@src/util/getRndEntries";
 import { isThisWeek, isToday } from "@src/util/isToday";
 import { getRndInt } from "@src/util/getRndInt";
 import { getIsoDate } from "@src/util/getIsoDate";
+// @ts-expect-error
+import { IS_ANDROID } from "@dataInterface/isAndroid";
 
 const MAX_ANSWERS = 4;
 const CENTER_INDEX = 4;
 
 export const getDashboardEntriesFromQuestions = (
   syncData: SyncData,
-  now = new Date(),
+  now = new Date()
 ): DashboardGroup[] => {
   const ds = getIsoDate(now);
   const dashboardGroups = [];
@@ -31,8 +33,8 @@ export const getDashboardEntriesFromQuestions = (
     ...FIXED_QUESTION_CATEGORIES_ON_DASHBOARD,
     ...getRndEntries(
       RANDOM_QUESTION_CATEGORIES_ON_DASHBOARD,
-      RANDOM_QUESTION_CATEGORIES_ON_DASHBOARD.length,
-    ),
+      RANDOM_QUESTION_CATEGORIES_ON_DASHBOARD.length
+    )
   ];
 
   groupsToCheck.forEach((catId) => {
@@ -41,15 +43,15 @@ export const getDashboardEntriesFromQuestions = (
       (answer) =>
         answer.questionCategoryId === catId &&
         (!category.isTodayOnlyCategory || isToday(answer.ts)) &&
-        (!category.isThisWeekOnlyCategory || isThisWeek(answer.ts)),
+        (!category.isThisWeekOnlyCategory || isThisWeek(answer.ts))
     );
-    if (answersForCat?.length) {
+    if(answersForCat?.length) {
       dashboardGroups.push({
         id: catId,
         dashboardTxt: QUESTION_CATEGORIES[catId].dashboardTxt,
         // TODO more sophisticated algorithm based on character length
         answers: getRndEntries(answersForCat, MAX_ANSWERS),
-        type: DashboardGroupType.TxtQuestion,
+        type: DashboardGroupType.TxtQuestion
       });
     }
   });
@@ -57,39 +59,39 @@ export const getDashboardEntriesFromQuestions = (
   const sortedEntries: DashboardGroup[] = dashboardGroups;
   let fixedEntriesIndexAndNr = 0;
 
-  if (syncData.sunTaps[ds] > 0) {
+  if(syncData.sunTaps[ds] > 0) {
     sortedEntries.splice(fixedEntriesIndexAndNr, 0, {
       type: DashboardGroupType.Stats,
       attempts: syncData.attempts[ds] || 0,
-      sunTaps: syncData.sunTaps[ds] || 0,
+      sunTaps: syncData.sunTaps[ds] || 0
     } as DashboardGroupStats);
     fixedEntriesIndexAndNr++;
   }
 
-  if (isToday(syncData.moodCheckTS)) {
+  if(isToday(syncData.moodCheckTS)) {
     sortedEntries.splice(fixedEntriesIndexAndNr, 0, {
       id: QuestionCategoryId.XMoodCheckin,
       type: DashboardGroupType.MoodCheckin,
       mood: syncData.moodCheckVal,
-      additionalTxt: syncData.moodCheckAdditional,
+      additionalTxt: syncData.moodCheckAdditional
     } as DashboardGroupMood);
     fixedEntriesIndexAndNr++;
   }
 
-  if (isToday(syncData.energyLvlTS)) {
+  if(isToday(syncData.energyLvlTS)) {
     sortedEntries.splice(fixedEntriesIndexAndNr, 0, {
       id: QuestionCategoryId.XEnergyLevelToday,
       type: DashboardGroupType.EnergyLvl,
-      energyLvl: syncData.energyLvlVal,
+      energyLvl: syncData.energyLvlVal
     } as DashboardGroupEnergyLvl);
     fixedEntriesIndexAndNr++;
   }
 
-  if (Object.keys(syncData.browsingBehaviorRating).length >= 3) {
+  if(Object.keys(syncData.browsingBehaviorRating).length >= 3) {
     sortedEntries.push({
       id: QuestionCategoryId.XBrowsingBehaviorHappiness,
       type: DashboardGroupType.BrowsingBehaviorRating,
-      data: syncData.browsingBehaviorRating,
+      data: syncData.browsingBehaviorRating
     } as DashboardGroupBrowsingBehavior);
     // sortedEntries.splice(ENERGY_LVL_INDEX, 0, {
     //   type: DashboardGroupType.BrowsingBehaviorRating,
@@ -98,18 +100,18 @@ export const getDashboardEntriesFromQuestions = (
   }
 
   // center one rnd entry
-  if (sortedEntries.length >= 5) {
+  if(sortedEntries.length >= 5 && !IS_ANDROID) {
     // NOTE: start val needs to be bigger than the fixed added entries
     const rndIndex = getRndInt(
       fixedEntriesIndexAndNr,
-      sortedEntries.length - 1,
+      sortedEntries.length - 1
     );
     const rndEntry = { ...sortedEntries[rndIndex] };
     sortedEntries.splice(rndIndex, 1);
-    sortedEntries.splice(CENTER_INDEX, 0, rndEntry);
+    sortedEntries.splice(CENTER_INDEX, 0, rndEntry as DashboardGroup);
   } else {
     sortedEntries.splice(CENTER_INDEX, 0, {
-      type: DashboardGroupType.Quote,
+      type: DashboardGroupType.Quote
     });
   }
 
