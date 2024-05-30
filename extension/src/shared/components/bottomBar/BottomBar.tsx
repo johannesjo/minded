01d3394960a7bb6315f52c57c2bc3b5a1dbef184
Johannes Millan <johannes.millan@gmail.com>
2024-05-30 22:32:52 +0200

    feat: switch settings and feedback

--- extension/src/shared/components/bottomBar/BottomBar.tsx
+++ extension/src/shared/components/bottomBar/BottomBar.tsx
@@ -1,13 +1,5 @@
 // @ts-expect-error
 import styles from "./BottomBar.module.scss";
-// @ts-expect-error
-import settingsSvg from "@assets/img/settings.svg";
-// @ts-expect-error
-import askQuestionSvg from "@assets/img/ask-question.svg";
-// @ts-expect-error
-import feedbackSvg from "@assets/img/feedback.svg";
-// @ts-expect-error
-import closeSvg from "@assets/img/close.svg";
 
 import { A, useLocation } from "@solidjs/router";
 import { createEffect, createSignal } from "solid-js";
@@ -37,20 +29,20 @@ const BottomBar = (props: { onShowQuestion: () => void }) => {
             <Ico name="askQuestion" />
           </button>
           <A
-            title="Go to settings page"
-            class="btnIcoOnly"
-            href="/settings"
-            activeClass="active"
-          >
-            <Ico name="settings" />
-          </A>
-          <A
             title="Give us somet feedback"
             class="btnIcoOnly"
             href="/feedback"
             activeClass="active"
           >
             <Ico name="feedback" />
+          </A>
+          <A
+            title="Go to settings page"
+            class="btnIcoOnly"
+            href="/settings"
+            activeClass="active"
+          >
+            <Ico name="settings" />
           </A>
         </>
       ) : (
