85f645b31c2f77aa037c0543b154597ca63d29e3
Johannes Millan <johannes.millan@gmail.com>
2024-05-30 22:48:46 +0200

    fix: typo

--- extension/src/shared/data/quotes.ts
+++ extension/src/shared/data/quotes.ts
@@ -9,7 +9,7 @@ export const QUOTES: Quote[] = [
     txt: "Sometimes your joy is the source of your smile, but sometimes your smile can be the source of your joy.",
   },
   {
-    txt: "Good is good enough. Don`t aim for perfection. Focus on the important things.",
+    txt: "Good is good enough. Don't aim for perfection. Focus on the important things.",
   },
   {
     txt: "Embrace Progress over Achievement. Productivity is not about massive strides every day but the accumulation of small, incremental progress that leads to significant achievements over time.",
