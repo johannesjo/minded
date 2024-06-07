1e147b03300f0b60dca52cd4d51eac847e76c63b
johannesjo <johannes.millan@gmail.com>
2024-06-07 17:48:33 +0200

    feat(ios): consider safe areas for ios

--- extension/src/ios/main/indexMainIOS.tsx
+++ extension/src/ios/main/indexMainIOS.tsx
@@ -1,4 +1,5 @@
 (window as any).IS_MAIN_MINDED_6622 = true;
+import { SafeArea, SafeAreaInsets } from "capacitor-plugin-safe-area";
 
 import { render } from "solid-js/web";
 import "./indexMainIOS.scss";
@@ -9,5 +10,24 @@ const appContainer = document.querySelector("#minded-6622");
 if (!appContainer) {
   throw new Error("Can not find AppContainer");
 }
+
+SafeArea.getSafeAreaInsets().then((data) => {
+  updateInsets(data);
+});
+SafeArea.addListener("safeAreaChanged", (data) => {
+  updateInsets(data);
+});
+
+const updateInsets = (data: SafeAreaInsets) => {
+  const { insets } = data;
+  console.log(data);
+
+  for (const [key, value] of Object.entries(insets)) {
+    (appContainer as HTMLElement).style.setProperty(
+      `--safe-area-inset-${key}`,
+      `${value}px`,
+    );
+  }
+};
 
 render(() => <MainIOS />, appContainer);
