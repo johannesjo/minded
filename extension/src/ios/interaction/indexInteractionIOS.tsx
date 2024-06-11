7a4088b1767062043442682b758c0b5672041e43
johannesjo <johannes.millan@gmail.com>
2024-06-11 09:22:00 +0200

    feat(ios): make safe areas for interaction

--- extension/src/ios/interaction/indexInteractionIOS.tsx
+++ extension/src/ios/interaction/indexInteractionIOS.tsx
@@ -2,10 +2,13 @@ import { render } from "solid-js/web";
 import "./indexInteractionIOS.scss";
 // @ts-ignore
 import InteractionIOS from "@src/ios/interaction/InteractionIOS.tsx";
+import { setupUpdateInsets } from "@src/ios/shared/setupUpdateInsets";
 
 const appContainer = document.querySelector("#minded-6622");
 if (!appContainer) {
   throw new Error("Can not find AppContainer");
 }
+
+setupUpdateInsets(appContainer as HTMLElement);
 
 render(InteractionIOS as any, appContainer);
--- extension/src/ios/main/indexMainIOS.tsx
+++ extension/src/ios/main/indexMainIOS.tsx
@@ -1,5 +1,6 @@
+import { setupUpdateInsets } from "@src/ios/shared/setupUpdateInsets";
+
 (window as any).IS_MAIN_MINDED_6622 = true;
-import { SafeArea, SafeAreaInsets } from "capacitor-plugin-safe-area";
 
 import { render } from "solid-js/web";
 import "./indexMainIOS.scss";
@@ -10,24 +11,5 @@ const appContainer = document.querySelector("#minded-6622");
 if (!appContainer) {
   throw new Error("Can not find AppContainer");
 }
-
-SafeArea.getSafeAreaInsets().then((data) => {
-  updateInsets(data);
-});
-SafeArea.addListener("safeAreaChanged", (data) => {
-  updateInsets(data);
-});
-
-const updateInsets = (data: SafeAreaInsets) => {
-  const { insets } = data;
-  console.log(data);
-
-  for (const [key, value] of Object.entries(insets)) {
-    (appContainer as HTMLElement).style.setProperty(
-      `--safe-area-inset-${key}`,
-      `${value}px`,
-    );
-  }
-};
-
+setupUpdateInsets(appContainer as HTMLElement);
 render(() => <MainIOS />, appContainer);
