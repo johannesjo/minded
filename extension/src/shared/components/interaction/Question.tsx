b1347b1ae435870c4238b00004faae19e3b1bcf2
Johannes Millan <johannes.millan@gmail.com>
2024-06-22 13:40:26 +0200

    feat: extract input as its own component

--- extension/src/shared/components/interaction/Question.tsx
+++ extension/src/shared/components/interaction/Question.tsx
@@ -1,16 +1,15 @@
 /* @refresh reload */
-import { createEffect, createSignal, JSX, onCleanup, onMount } from "solid-js";
+import { createEffect, createSignal, JSX, onCleanup } from "solid-js";
 import { QuestionForPrompt } from "@src/shared/data/questions";
 import { Answer } from "@src/dataInterface/syncData";
 import { saveAnswer } from "@src/dataInterface/commonSyncDataInterface";
-// @ts-expect-error
-import { requestFocusAndShowKeyboard } from "@dataInterface/system";
 import { nanoid } from "nanoid";
 import {
   getQuestionSemiSmart,
   getQuestionSmart,
 } from "@src/util/getQuestionSmart";
 import { Ico } from "@src/shared/components/ui/Ico";
+import { InputWithSend } from "@src/shared/components/ui/InputWithSend";
 
 const MAX_SMART_QUESTION_ATTEMPTS = 3;
 
@@ -26,45 +25,27 @@ export const Question: (props: {
   const [getIsInputDisabled, setIsInputDisabled] = createSignal(false);
   const [getQuestion, setQuestion] = createSignal(props.initialQuestion);
   const [getIsChangingQuestion, setIsChangingQuestion] = createSignal(false);
-  let inpEl;
-  let t0;
-  let t1;
+  const [getInpEl, setInpEl] = createSignal<HTMLInputElement | null>(null);
+  const [getValue, setValue] = createSignal<string>("");
+
   let tChangeQuestion;
 
   let questionUpdateCount = 0;
   let questionIdBefore = props.initialQuestion.id;
 
+  onCleanup(() => {
+    window.clearTimeout(tChangeQuestion);
+  });
+
   createEffect(() => {
-    if (inpEl) {
-      inpEl.value = props.initialQuestion.prompt
-        ? props.initialQuestion.prompt + " "
-        : "";
-    }
+    setValue(
+      props.initialQuestion.prompt ? props.initialQuestion.prompt + " " : "",
+    );
   });
 
   createEffect(() => {
     const q = getQuestion();
-    if (inpEl) {
-      inpEl.value = q.prompt ? q.prompt + " " : "";
-    }
-  });
-
-  const focusInp = () => {
-    inpEl.focus();
-    requestFocusAndShowKeyboard();
-  };
-
-  onMount(async () => {
-    focusInp();
-    // updatePrompt();
-    t0 = setTimeout(() => focusInp(), 200);
-    t1 = setTimeout(() => focusInp(), 600);
-  });
-
-  onCleanup(() => {
-    window.clearTimeout(t0);
-    window.clearTimeout(t1);
-    window.clearTimeout(tChangeQuestion);
+    setValue(q.prompt ? q.prompt + " " : "");
   });
 
   const submitAnswer = async (answerTxt: string) => {
@@ -88,9 +69,6 @@ export const Question: (props: {
   };
 
   const onKeyDown = (ev: KeyboardEvent): void => {
-    const q = props.initialQuestion;
-    if (!q) throw new Error("No initialQuestion");
-
     if (ev.key === "Enter") {
       submitAnswer((ev.target as HTMLInputElement).value);
     } else if (ev.key === "Escape") {
@@ -127,26 +105,20 @@ export const Question: (props: {
           <span>{getQuestion().t}?</span>
         </div>
 
-        <div id="minded-6622-inp">
-          <input
-            ref={inpEl}
-            type="text"
-            disabled={getIsInputDisabled()}
-            onkeypress={() => undefined}
-            onmouseenter={props.onCancelCountdown}
-            onclick={props.onCancelCountdown}
-            onkeydown={onKeyDown}
-            maxlength={500}
-            autofocus={true}
+        <div
+          onmouseenter={props.onCancelCountdown}
+          onclick={props.onCancelCountdown}
+        >
+          <InputWithSend
+            onKeyDown={onKeyDown}
+            value={getValue()}
+            isAutoFocus={true}
+            isDisabled={getIsInputDisabled()}
+            setRef={setInpEl}
+            onSubmitClick={submitAnswer}
           />
-          <div
-            onclick={() => {
-              submitAnswer(inpEl?.value);
-            }}
-          >
-            <Ico name="send" />
-          </div>
         </div>
+
         {props.isChangeQuestion && (
           <div
             id="minded-6622-change-question-btn"
@@ -155,10 +127,10 @@ export const Question: (props: {
             onclick={() => {
               props.onCancelCountdown();
               setIsChangingQuestion(true);
-              inpEl?.focus();
+              getInpEl()?.focus();
               tChangeQuestion = window.setTimeout(() => {
                 updateQuestion();
-                inpEl?.focus();
+                getInpEl()?.focus();
                 setIsChangingQuestion(false);
               }, 100);
             }}
