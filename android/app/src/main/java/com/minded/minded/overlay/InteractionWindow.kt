92fcf6bd4cdd86fa8f946e71c008e11eaa17427b
Johannes Millan <johannes.millan@gmail.com>
2025-12-06 12:51:38 +0100

    fix(android): extend overlay behind navigation bar
    Add SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION to eliminate gap at bottom
    of interaction overlay.

--- android/app/src/main/java/com/minded/minded/overlay/InteractionWindow.kt
+++ android/app/src/main/java/com/minded/minded/overlay/InteractionWindow.kt
@@ -100,7 +100,8 @@ class InteractionWindow(
             // This is different from the view background and helps during transitions
             @Suppress("DEPRECATION")
             this.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         }
     }
 }
