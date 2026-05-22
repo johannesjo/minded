22c58a10762a507d7158e010e00701709428c897
Johannes Millan <johannes.millan@gmail.com>
2026-05-22 17:13:36 +0200

    chore(release): bump version to 4.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.minded.minded"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10
-        versionName = "4.0.0"
+        versionCode = 11
+        versionName = "4.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
