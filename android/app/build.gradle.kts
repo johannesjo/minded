7bfa91ac34d264db5c9d56b22d9df5c19e97f7b6
Johannes Millan <johannes.millan@gmail.com>
2026-05-27 00:36:40 +0200

    chore(release): bump version to 5.0.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.minded.minded"
         minSdk = 29
         targetSdk = 35
-        versionCode = 14
-        versionName = "5.0.2"
+        versionCode = 15
+        versionName = "5.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
