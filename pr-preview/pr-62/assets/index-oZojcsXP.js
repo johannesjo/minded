80678fe17c33ff9843b3c1ad67f301e48dc73b48
johannesjo <johannesjo@users.noreply.github.com>
2026-06-22 22:47:09 +0000

    Deploy preview for PR 62 🛫

---
+++ pr-preview/pr-62/assets/index-oZojcsXP.js
@@ -0,0 +1,1 @@
+import{P as n,aR as o,a0 as r,z as t}from"./Styleguide-DsxQlOw7.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
