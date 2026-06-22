9802d27383ea869c86842a62e2b16768cc32fa9a
johannesjo <johannesjo@users.noreply.github.com>
2026-06-22 09:21:14 +0000

    Remove preview for PR 36 🛬

--- pr-preview/pr-36/assets/index-BRKbyeY6.js
+++
@@ -1,1 +0,0 @@
-import{U as n,aR as o,a1 as r,z as t}from"./Styleguide-FAEj4lab.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
