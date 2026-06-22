78cc6ad51b73f8e8e62105959b14effcf84da0a4
johannesjo <johannesjo@users.noreply.github.com>
2026-06-22 09:49:25 +0000

    Deploy preview for PR 46 🛫

--- pr-preview/pr-46/assets/index-DpbkbSuX.js
+++
@@ -1,1 +0,0 @@
-import{P as n,aQ as o,a0 as r,z as t}from"./Styleguide-BxedMTXD.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
