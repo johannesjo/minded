bec2c169011c6c5c16cc58f6f5aaea27f7a3da23
johannesjo <johannesjo@users.noreply.github.com>
2026-06-23 20:55:49 +0000

    Deploy preview for PR 67 🛫

---
+++ pr-preview/pr-67/assets/index-D0pwI1W7.js
@@ -0,0 +1,1 @@
+import{O as n,aR as o,$ as r,y as t}from"./Styleguide-D91f3Bq3.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
