47799ba8c305ac10a8e9e0e3f28576605e727dd7
johannesjo <johannesjo@users.noreply.github.com>
2026-06-23 22:45:36 +0000

    Deploy preview for PR 74 🛫

---
+++ pr-preview/pr-74/assets/index-C3Vtien1.js
@@ -0,0 +1,1 @@
+import{W as n,aV as o,a3 as r,H as t}from"./Styleguide-COKqhPMA.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
