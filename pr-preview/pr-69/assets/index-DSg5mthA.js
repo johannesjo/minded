b07f8f6edaf2f3768a9908d84441fa921bf6e0b6
johannesjo <johannesjo@users.noreply.github.com>
2026-06-23 21:03:18 +0000

    Deploy preview for PR 69 🛫

---
+++ pr-preview/pr-69/assets/index-DSg5mthA.js
@@ -0,0 +1,1 @@
+import{U as n,aT as o,a1 as r,A as t}from"./Styleguide-CEjl_69h.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
