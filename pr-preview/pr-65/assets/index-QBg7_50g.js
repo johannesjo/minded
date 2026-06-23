634dde64b31816f8aff83be7c59467bafcea3643
johannesjo <johannesjo@users.noreply.github.com>
2026-06-23 18:57:23 +0000

    Deploy preview for PR 65 🛫

---
+++ pr-preview/pr-65/assets/index-QBg7_50g.js
@@ -0,0 +1,1 @@
+import{U as n,aS as o,a1 as r,A as t}from"./Styleguide-42AOXLE4.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
