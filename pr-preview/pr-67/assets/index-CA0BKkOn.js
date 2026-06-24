5aabd6addf39703b3605ca5d647122c6f3356111
johannesjo <johannesjo@users.noreply.github.com>
2026-06-24 08:47:10 +0000

    Deploy preview for PR 67 🛫

--- pr-preview/pr-67/assets/index-CA0BKkOn.js
+++
@@ -1,1 +0,0 @@
-import{P as n,aT as o,a0 as r,z as t}from"./Styleguide-DBwKSvYN.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
