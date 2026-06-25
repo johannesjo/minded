5520f58db23911ecb800a8dab27414a8d936b584
johannesjo <johannesjo@users.noreply.github.com>
2026-06-25 14:16:29 +0000

    Deploy preview for PR 87 🛫

--- pr-preview/pr-87/assets/index-BikZDXqs.js
+++
@@ -1,1 +0,0 @@
-import{V as n,aV as o,a2 as r,E as t}from"./Styleguide-CeKe7EF2.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
