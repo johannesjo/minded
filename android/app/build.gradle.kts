58ca635240306abe87a6227820994191cc4b862a
Johannes Millan <johannes.millan@gmail.com>
2026-06-25 17:42:55 +0200

    chore(release): bump version to 6.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.minded.minded"
         minSdk = 29
         targetSdk = 35
-        versionCode = 21
-        versionName = "6.0.0"
+        versionCode = 22
+        versionName = "6.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
