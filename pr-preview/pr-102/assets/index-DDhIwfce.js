554c4c775f3a90cc55bc60138ee0e4b0d27a61ad
johannesjo <johannesjo@users.noreply.github.com>
2026-06-26 22:04:22 +0000

    Deploy preview for PR 102 🛫

---
+++ pr-preview/pr-102/assets/index-DDhIwfce.js
@@ -0,0 +1,1 @@
+import{V as n,aW as o,a2 as r,E as t}from"./Styleguide-CmCvpfp0.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
