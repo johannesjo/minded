439707fb27ddd9ced6c9eec2e3865c5dc0cc77f0
johannesjo <johannesjo@users.noreply.github.com>
2026-06-27 22:44:14 +0000

    Deploy preview for PR 110 🛫

---
+++ pr-preview/pr-110/assets/index-CLt_31__.js
@@ -0,0 +1,1 @@
+import{V as n,aX as o,a2 as r,E as t}from"./Styleguide-B5scgxQK.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
