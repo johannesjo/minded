4edcc6047239f570967c3781cd6e572a9769c2ed
johannesjo <johannesjo@users.noreply.github.com>
2026-06-30 23:30:00 +0000

    Deploy preview for PR 113 🛫

---
+++ pr-preview/pr-113/assets/index-CPS2tiK-.js
@@ -0,0 +1,1 @@
+import{W as n,aY as o,a2 as r,H as t}from"./Styleguide-Cr-BoZQb.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
