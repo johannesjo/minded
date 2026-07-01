4bf846fbb9a6de060e6d143838214e4748217242
johannesjo <johannesjo@users.noreply.github.com>
2026-07-01 09:38:02 +0000

    Remove preview for PR 113 🛬

--- pr-preview/pr-113/assets/index-CPS2tiK-.js
+++
@@ -1,1 +0,0 @@
-import{W as n,aY as o,a2 as r,H as t}from"./Styleguide-Cr-BoZQb.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
