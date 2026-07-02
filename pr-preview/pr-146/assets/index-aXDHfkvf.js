3d4287f0cde2e3d1061868e7a3143e32c9964319
johannesjo <johannesjo@users.noreply.github.com>
2026-07-02 22:59:57 +0000

    Deploy preview for PR 146 🛫

---
+++ pr-preview/pr-146/assets/index-aXDHfkvf.js
@@ -0,0 +1,1 @@
+import{K as n,aU as o,_ as r,y as t}from"./Styleguide-C65JldZf.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
