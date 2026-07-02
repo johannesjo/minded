808b70498c6b5e58827016be1c21d7521dda5984
johannesjo <johannesjo@users.noreply.github.com>
2026-07-02 10:52:47 +0000

    Deploy preview for PR 133 🛫

---
+++ pr-preview/pr-133/assets/index-Dgy83QTb.js
@@ -0,0 +1,1 @@
+import{W as n,aZ as o,a3 as r,H as t}from"./Styleguide-DOpdEDSA.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
