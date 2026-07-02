ef12dc234e4ab2a597822a92824bd2d579f12cbd
johannesjo <johannesjo@users.noreply.github.com>
2026-07-02 23:14:40 +0000

    Deploy preview for PR 145 🛫

---
+++ pr-preview/pr-145/assets/index-C1AAYevr.js
@@ -0,0 +1,1 @@
+import{O as n,aW as o,$ as r,z as t}from"./Styleguide-BdGL9uNF.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
