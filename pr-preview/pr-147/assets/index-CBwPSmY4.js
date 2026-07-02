9ccc13a41e7764c9a897ea8299a2bd36a0e9f654
johannesjo <johannesjo@users.noreply.github.com>
2026-07-02 23:18:47 +0000

    Remove preview for PR 147 🛬

--- pr-preview/pr-147/assets/index-CBwPSmY4.js
+++
@@ -1,1 +0,0 @@
-import{O as n,aV as o,$ as r,z as t}from"./Styleguide-CRMbzfqD.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
