cbda44dc24b558d1ec164dd0ca89b1bee113169d
johannesjo <johannesjo@users.noreply.github.com>
2026-07-02 22:54:40 +0000

    Deploy preview for PR 145 🛫

---
+++ pr-preview/pr-145/assets/index-DMovRgJO.js
@@ -0,0 +1,1 @@
+import{X as n,a$ as o,a4 as r,J as t}from"./Styleguide-DmlhUryR.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
