ed82bb3ec0f783741ab233242141319ee14191e8
johannesjo <johannesjo@users.noreply.github.com>
2026-07-02 16:05:59 +0000

    Remove preview for PR 134 🛬

--- pr-preview/pr-134/assets/index-BySx2wfk.js
+++
@@ -1,1 +0,0 @@
-import{W as n,aZ as o,a3 as r,H as t}from"./Styleguide-Cr6KGHyj.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
