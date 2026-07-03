474d9c53ea901f7227af559caa1d456dd377c42b
johannesjo <johannesjo@users.noreply.github.com>
2026-07-03 22:17:08 +0000

    Remove preview for PR 174 🛬

--- pr-preview/pr-174/assets/index-Cx5pNkGd.js
+++
@@ -1,1 +0,0 @@
-import{P as n,aY as o,a0 as r,A as t}from"./Styleguide-l174sdvM.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
