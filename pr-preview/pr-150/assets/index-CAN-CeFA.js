95d37a4ee7f1b197d00181be5a722bc2ae5274d3
johannesjo <johannesjo@users.noreply.github.com>
2026-07-03 11:16:08 +0000

    Remove preview for PR 150 🛬

--- pr-preview/pr-150/assets/index-CAN-CeFA.js
+++
@@ -1,1 +0,0 @@
-import{P as n,aY as o,a0 as r,A as t}from"./Styleguide-2F0Ws2ag.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
