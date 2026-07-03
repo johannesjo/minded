f377eccd9fe0589361bb5b51607db4dd1d80155c
johannesjo <johannesjo@users.noreply.github.com>
2026-07-03 22:18:07 +0000

    Remove preview for PR 175 🛬

--- pr-preview/pr-175/assets/index-B0QtygbC.js
+++
@@ -1,1 +0,0 @@
-import{P as n,aZ as o,a0 as r,A as t}from"./Styleguide-CQLEatG2.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
