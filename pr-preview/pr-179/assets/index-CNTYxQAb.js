1855f7249a8cce92382f4d2b2dcdbaf40926e11b
johannesjo <johannesjo@users.noreply.github.com>
2026-07-03 23:12:29 +0000

    Deploy preview for PR 179 🛫

---
+++ pr-preview/pr-179/assets/index-CNTYxQAb.js
@@ -0,0 +1,1 @@
+import{P as n,aZ as o,a0 as r,A as t}from"./Styleguide-Cmb2S3VO.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
