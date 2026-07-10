20e8efaff220e720ef63a93048d3ceb5c3f6eb17
johannesjo <johannesjo@users.noreply.github.com>
2026-07-10 00:13:36 +0000

    Deploy preview for PR 205 🛫

---
+++ pr-preview/pr-205/assets/index-ZaVpYf21.js
@@ -0,0 +1,1 @@
+import{V as n,a$ as o,a2 as r,H as t}from"./Styleguide-P_MkyCId.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
