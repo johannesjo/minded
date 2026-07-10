0aa01613829b69ba690a6d7ac3628d5319961db7
johannesjo <johannesjo@users.noreply.github.com>
2026-07-10 21:59:14 +0000

    Remove preview for PR 206 🛬

--- pr-preview/pr-206/assets/index-C1_jHFp8.js
+++
@@ -1,1 +0,0 @@
-import{V as n,a$ as o,a2 as r,H as t}from"./Styleguide-Dsf4Iias.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
