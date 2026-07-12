4f9998825eaaea2bba912251083e7b11132e2e49
johannesjo <johannesjo@users.noreply.github.com>
2026-07-12 11:58:30 +0000

    Remove preview for PR 214 🛬

--- pr-preview/pr-214/assets/index-l0wSkmSx.js
+++
@@ -1,1 +0,0 @@
-import{V as n,a$ as o,a2 as r,H as t}from"./Styleguide-BXr09wt0.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
