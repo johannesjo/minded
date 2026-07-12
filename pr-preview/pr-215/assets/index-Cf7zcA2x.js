e8cd28590f9ae025bbdc66010b86a50e2f277c44
johannesjo <johannesjo@users.noreply.github.com>
2026-07-12 21:31:23 +0000

    Deploy preview for PR 215 🛫

---
+++ pr-preview/pr-215/assets/index-Cf7zcA2x.js
@@ -0,0 +1,1 @@
+import{W as n,b0 as o,a2 as r,J as t}from"./Styleguide-OZ5EUd_f.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
