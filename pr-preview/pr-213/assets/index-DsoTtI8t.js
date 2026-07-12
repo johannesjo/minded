2fd94d0cae7e256908040bb12cb5992b3872a496
johannesjo <johannesjo@users.noreply.github.com>
2026-07-12 11:56:40 +0000

    Remove preview for PR 213 🛬

--- pr-preview/pr-213/assets/index-DsoTtI8t.js
+++
@@ -1,1 +0,0 @@
-import{W as n,b0 as o,a3 as r,J as t}from"./Styleguide-D1K-Qoji.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
