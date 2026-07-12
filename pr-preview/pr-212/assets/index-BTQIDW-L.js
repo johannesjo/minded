88c35763ad91ee8e62fc195b3f0d5980cb45eff3
johannesjo <johannesjo@users.noreply.github.com>
2026-07-12 02:24:55 +0000

    Deploy preview for PR 212 🛫

---
+++ pr-preview/pr-212/assets/index-BTQIDW-L.js
@@ -0,0 +1,1 @@
+import{V as n,a_ as o,a1 as r,H as t}from"./Styleguide-CEDmIB8q.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
