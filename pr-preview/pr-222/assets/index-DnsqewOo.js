a3fb3d61a08665d26d63c103482e231bae7a83c5
johannesjo <johannesjo@users.noreply.github.com>
2026-07-13 20:09:27 +0000

    Deploy preview for PR 222 🛫

---
+++ pr-preview/pr-222/assets/index-DnsqewOo.js
@@ -0,0 +1,1 @@
+import{Y as n,b2 as o,a4 as r,O as t}from"./Styleguide-DVA4SsZ4.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
