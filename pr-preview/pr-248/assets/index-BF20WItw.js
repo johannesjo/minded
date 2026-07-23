279d75355a47c044669628ac5db5a3477df95267
johannesjo <johannesjo@users.noreply.github.com>
2026-07-23 20:03:32 +0000

    Deploy preview for PR 248 🛫

---
+++ pr-preview/pr-248/assets/index-BF20WItw.js
@@ -0,0 +1,1 @@
+import{Y as n,b3 as o,a4 as r,O as t}from"./Styleguide-CrDfZRkE.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
