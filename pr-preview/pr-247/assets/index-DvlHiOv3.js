ec1ef3969e19f1a441387c5b9742426de458c470
johannesjo <johannesjo@users.noreply.github.com>
2026-07-23 00:31:27 +0000

    Deploy preview for PR 247 🛫

---
+++ pr-preview/pr-247/assets/index-DvlHiOv3.js
@@ -0,0 +1,1 @@
+import{Y as n,b3 as o,a4 as r,O as t}from"./Styleguide-0A3Z8v3Q.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
