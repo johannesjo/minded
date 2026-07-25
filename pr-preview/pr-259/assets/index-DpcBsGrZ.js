59e73456d02a55e538db959ed3b4c689d8525d0e
johannesjo <johannesjo@users.noreply.github.com>
2026-07-25 11:40:17 +0000

    Deploy preview for PR 259 🛫

---
+++ pr-preview/pr-259/assets/index-DpcBsGrZ.js
@@ -0,0 +1,1 @@
+import{Y as n,b3 as o,a4 as r,O as t}from"./Styleguide-D2H8cp-i.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
