110a5eda8ed2d957a54202f467775791f8e5057a
johannesjo <johannesjo@users.noreply.github.com>
2026-07-28 20:09:09 +0000

    Deploy preview for PR 266 🛫

--- pr-preview/pr-266/assets/index-D_pYVyL1.js
+++
@@ -1,1 +0,0 @@
-import{Y as n,b8 as o,a4 as r,O as t}from"./Styleguide-DOeaTPLg.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
