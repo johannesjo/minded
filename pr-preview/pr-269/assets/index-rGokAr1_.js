091db7e0a08f27eb41fd081100210c06bc602217
johannesjo <johannesjo@users.noreply.github.com>
2026-07-28 20:39:26 +0000

    Deploy preview for PR 269 🛫

--- pr-preview/pr-269/assets/index-rGokAr1_.js
+++
@@ -1,1 +0,0 @@
-import{Z as n,ba as o,a5 as r,P as t}from"./Styleguide-BsUSlMm2.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
