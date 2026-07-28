74c1571b5135c21420193eb468e24fe2cd8a7bd0
johannesjo <johannesjo@users.noreply.github.com>
2026-07-28 20:04:29 +0000

    Deploy preview for PR 270 🛫

---
+++ pr-preview/pr-270/assets/index-hFpNYRR5.js
@@ -0,0 +1,1 @@
+import{Y as n,b8 as o,a4 as r,O as t}from"./Styleguide-CZ5At-t-.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
