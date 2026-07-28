30e4551a78052331fff74313d9732c829e77d986
johannesjo <johannesjo@users.noreply.github.com>
2026-07-28 20:29:35 +0000

    Deploy preview for PR 268 🛫

---
+++ pr-preview/pr-268/assets/index-BQGy8wlS.js
@@ -0,0 +1,1 @@
+import{Y as n,b9 as o,a5 as r,O as t}from"./Styleguide-CJNwYOsn.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
