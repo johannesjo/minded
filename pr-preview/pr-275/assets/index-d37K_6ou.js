a33960781d3c52e9af7a7aeeb43c6e1161e7bd74
johannesjo <johannesjo@users.noreply.github.com>
2026-07-29 21:57:56 +0000

    Deploy preview for PR 275 🛫

---
+++ pr-preview/pr-275/assets/index-d37K_6ou.js
@@ -0,0 +1,1 @@
+import{_ as n,bc as o,a7 as r,T as t}from"./Styleguide-BmyDHPjd.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
