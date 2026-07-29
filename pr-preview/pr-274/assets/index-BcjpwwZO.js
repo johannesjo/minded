7a285b5d451efa7de314adf6fea2296e1e049011
johannesjo <johannesjo@users.noreply.github.com>
2026-07-29 11:11:48 +0000

    Remove preview for PR 274 🛬

--- pr-preview/pr-274/assets/index-BcjpwwZO.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,bc as o,a7 as r,T as t}from"./Styleguide-C3BhPPDk.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
