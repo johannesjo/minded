910a8c11e48d2c7461371deaa17988e6a699b4d5
johannesjo <johannesjo@users.noreply.github.com>
2026-09-24 21:25:08 +0000

    Remove preview for PR 283 🛬

--- pr-preview/pr-283/assets/index-fsu4XSXP.js
+++
@@ -1,1 +0,0 @@
-import{$ as n,bj as o,aa as r,U as t}from"./Styleguide-DtR_Ijgy.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
