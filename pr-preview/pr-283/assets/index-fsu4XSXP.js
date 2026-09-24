afc0009b77289f1183a0f53093ebb377132866e2
johannesjo <johannesjo@users.noreply.github.com>
2026-09-24 21:20:55 +0000

    Deploy preview for PR 283 🛫

---
+++ pr-preview/pr-283/assets/index-fsu4XSXP.js
@@ -0,0 +1,1 @@
+import{$ as n,bj as o,aa as r,U as t}from"./Styleguide-DtR_Ijgy.js";const e=document.getElementById("minded-6622");if(!e)throw new Error("Cannot find #minded-6622 root element");n();o(()=>r(t,{}),e);
